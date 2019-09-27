087210d118e95b7ad92bec6fe1e47056a9a1f2b6
amirphl <amirphl4@gmail.com>
2019-09-27 16:05:22 +0330

    toturial 7 completed

--- polls/models.py
+++ polls/models.py
@@ -14,6 +14,10 @@ class Question(models.Model):
         x = timezone.now()
         return x >= self.pub_date >= x - datetime.timedelta(days=1)
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
